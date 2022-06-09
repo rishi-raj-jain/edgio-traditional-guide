61bc8efce08289f1a4cf492d6aab75a9504d74fd
Rishi Raj Jain <46300090+rishi-raj-jain@users.noreply.github.com>
2022-06-09 10:46:21 +0530

    Redirect from homepage

---
+++ components/Redirect.vue
@@ -0,0 +1,13 @@
+<template></template>
+
+<script>
+import { defineComponent, onMounted } from '@vue/composition-api'
+
+export default defineComponent({
+  setup() {
+    onMounted(() => {
+      window.location.href = '/getting-started/installation'
+    })
+  },
+})
+</script>
