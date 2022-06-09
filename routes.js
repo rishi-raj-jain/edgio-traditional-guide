1f205934447dad95131da8da50e4392669d26e83
Rishi Raj Jain <46300090+rishi-raj-jain@users.noreply.github.com>
2022-06-09 12:23:42 +0530

    Add file/folder icons

--- routes.js
+++ routes.js
@@ -1,13 +1,17 @@
 import { Router } from '@layer0/core/router'
 
-export default new Router().static('dist', ({ cache }) => {
-  cache({
-    edge: {
-      maxAgeSeconds: 60 * 60 * 24 * 365,
-      forcePrivateCaching: true,
-    },
-    browser: {
-      serviceWorkerSeconds: 60 * 60 * 24 * 365,
-    },
+export default new Router()
+  .static('dist', ({ cache }) => {
+    cache({
+      edge: {
+        maxAgeSeconds: 60 * 60 * 24 * 365,
+        forcePrivateCaching: true,
+      },
+      browser: {
+        serviceWorkerSeconds: 60 * 60 * 24 * 365,
+      },
+    })
   })
-})
+  .fallback(({ serveStatic }) => {
+    serveStatic('dist/index.html')
+  })
